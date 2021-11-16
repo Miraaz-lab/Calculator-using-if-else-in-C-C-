239c7591fff9fdd461d35463ec195672d99dc1fc
Miraaz-lab <70813546+Miraaz-lab@users.noreply.github.com>
2021-11-16 14:55:29 +0530

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,44 @@
+#include <stdio.h>  
+//#include <conio.h>
+void main()  
+{  
+    float num1,num2;
+    char a;
+    //clrscr();
+    start:
+    printf(
+    "\n1.'+'Addition"
+    "\n2.'-'Substraction"
+    "\n3.'*'Multiplication"
+    "\n4.'/'Division");
+    printf("\nEnter the task you want to do  \n");
+    scanf("%c",&a);
+    
+    printf("Enter first value:\n");
+    scanf("%f",&num1);
+    
+    printf("Enter second value:\n");
+    scanf("%f",&num2);
+    
+    /*if (a!='+','-','*','/')
+    {goto start;}*/
+    
+    if (a=='+')
+    printf("sum of both number is: %.3f\n",num1+num2);
+    
+    else if(a=='-')
+    printf("substraction of values is: %.3f\n",num1-num2);
+    
+    else if(a=='*')
+    printf("multiplication of values is: %.3f\n",num1*num2);
+    
+    else if (a=='/')
+    printf("division of values is: %.3f\n",num1/num2);
+   
+    else {
+    printf("\nplease select proper oprator\n:");
+    scanf("%c",&a);
+    goto start;
+    }
+    //getch();
+}
